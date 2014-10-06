0293fab3e0f8536c4f8510ef71a9bb73ddba03e9
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-06 15:29:40 +0200

    Updated the usage of blas

--- src/lib.rs
+++ src/lib.rs
@@ -3,9 +3,9 @@ extern crate blas;
 #[inline]
 pub fn multiply(a: *const f64, b: *const f64, c: *mut f64, m: uint, p: uint, n: uint) {
     if n == 1 {
-        blas::dgemv(blas::NORMAL, m as i32, p as i32, 1.0, a, m as i32, b, 1, 0.0, c, 1);
+        blas::dgemv('N' as i8, m as i32, p as i32, 1.0, a, m as i32, b, 1, 0.0, c, 1);
     } else {
-        blas::dgemm(blas::NORMAL, blas::NORMAL, m as i32, n as i32, p as i32,
+        blas::dgemm('N' as i8, 'N' as i8, m as i32, n as i32, p as i32,
             1.0, a, m as i32, b, p as i32, 0.0, c, m as i32);
     }
 }
@@ -21,9 +21,9 @@ pub fn multiply_add(a: *const f64, b: *const f64, c: *const f64, d: *mut f64,
     }
 
     if n == 1 {
-        blas::dgemv(blas::NORMAL, m as i32, p as i32, 1.0, a, m as i32, b, 1, 1.0, d, 1);
+        blas::dgemv('N' as i8, m as i32, p as i32, 1.0, a, m as i32, b, 1, 1.0, d, 1);
     } else {
-        blas::dgemm(blas::NORMAL, blas::NORMAL, m as i32, n as i32, p as i32,
+        blas::dgemm('N' as i8, 'N' as i8, m as i32, n as i32, p as i32,
             1.0, a, m as i32, b, p as i32, 1.0, d, m as i32);
     }
 }
