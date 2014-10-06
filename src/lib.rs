7afdda1bf8f4058937b2491d485682297fabc605
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-06 21:33:16 +0200

    Updated the usage of blas and lapack

--- src/lib.rs
+++ src/lib.rs
@@ -8,10 +8,9 @@ extern crate lapack;
 #[inline]
 pub fn multiply(a: *const f64, b: *const f64, c: *mut f64, m: uint, p: uint, n: uint) {
     if n == 1 {
-        blas::dgemv('N' as i8, m as i32, p as i32, 1.0, a, m as i32, b, 1, 0.0, c, 1);
+        blas::dgemv(b'N', m, p, 1.0, a, m, b, 1, 0.0, c, 1);
     } else {
-        blas::dgemm('N' as i8, 'N' as i8, m as i32, n as i32, p as i32, 1.0, a,
-                    m as i32, b, p as i32, 0.0, c, m as i32);
+        blas::dgemm(b'N', b'N', m, n, p, 1.0, a, m, b, p, 0.0, c, m);
     }
 }
 
@@ -29,10 +28,9 @@ pub fn multiply_add(a: *const f64, b: *const f64, c: *const f64, d: *mut f64,
     }
 
     if n == 1 {
-        blas::dgemv('N' as i8, m as i32, p as i32, 1.0, a, m as i32, b, 1, 1.0, d, 1);
+        blas::dgemv(b'N', m, p, 1.0, a, m, b, 1, 1.0, d, 1);
     } else {
-        blas::dgemm('N' as i8, 'N' as i8, m as i32, n as i32, p as i32, 1.0, a,
-                    m as i32, b, p as i32, 1.0, d, m as i32);
+        blas::dgemm(b'N', b'N', m, n, p, 1.0, a, m, b, p, 1.0, d, m);
     }
 }
 
@@ -56,11 +54,10 @@ pub fn sym_eig(a: *const f64, vecs: *mut f64, vals: *mut f64, m: uint)
     let mut temp = Vec::from_elem(4 * m, 0.0);
     let mut flag = 0;
 
-    lapack::dsyev('V' as i8, 'U' as i8, m as i32, vecs, m as i32, vals,
-                  temp.as_mut_ptr(), 4 * m as i32, &mut flag);
+    lapack::dsyev(b'V', b'U', m, vecs, m, vals, temp.as_mut_ptr(), 4 * m, &mut flag);
 
     match flag {
         0 => Ok(()),
-        _ => Err(flag as int),
+        _ => Err(flag),
     }
 }
