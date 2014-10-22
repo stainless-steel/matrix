85809682765e95db577e99904d1e80e877eb37ae
Ivan Ukhov <ivan.ukhov@gmail.com>
2014-10-22 08:11:08 +0400

    Cosmetic adjustments

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-//! The library provides algorithms for manipulating real matrices.
+//! Algorithms for manipulating real matrices.
 
 extern crate blas;
 extern crate lapack;
@@ -8,9 +8,7 @@ extern crate lapack;
 /// An `m`-by-`p` matrix `a` is multiplied by a `p`-by-`n` matrix `b`; the
 /// result is stored in an `m`-by-`n` matrix `c`.
 #[inline]
-pub fn multiply(a: *const f64, b: *const f64, c: *mut f64, m: uint, p: uint,
-                n: uint) {
-
+pub fn multiply(a: *const f64, b: *const f64, c: *mut f64, m: uint, p: uint, n: uint) {
     if n == 1 {
         blas::dgemv(b'N', m, p, 1.0, a, m, b, 1, 0.0, c, 1);
     } else {
@@ -45,9 +43,7 @@ pub fn multiply_add(a: *const f64, b: *const f64, c: *const f64, d: *mut f64,
 /// A symmetric `m`-by-`m` matrix `a` is decomposed; the resulting eigenvectors
 /// and eigenvalus are stored in an `m`-by-`m` matrix `vecs` and an `m`-element
 /// vector `vals`, respectively.
-pub fn sym_eig(a: *const f64, vecs: *mut f64, vals: *mut f64, m: uint)
-               -> Result<(), int> {
-
+pub fn sym_eig(a: *const f64, vecs: *mut f64, vals: *mut f64, m: uint) -> Result<(), int> {
     if a != (vecs as *const f64) {
         // NOTE: Only the upper triangular matrix is actually needed; however,
         // copying only that part might not be optimal for performance. Check!
@@ -56,12 +52,12 @@ pub fn sym_eig(a: *const f64, vecs: *mut f64, vals: *mut f64, m: uint)
         }
     }
 
-    // The size of the temporary array should have at least 3 * m - 1.
+    // The size of the temporary array should be >= max(1, 3 * m - 1).
+    // http://www.netlib.org/lapack/explore-html/dd/d4c/dsyev_8f.html
     let mut temp = Vec::from_elem(4 * m, 0.0);
     let mut flag = 0;
 
-    lapack::dsyev(b'V', b'U', m, vecs, m, vals, temp.as_mut_ptr(), 4 * m,
-                  &mut flag);
+    lapack::dsyev(b'V', b'U', m, vecs, m, vals, temp.as_mut_ptr(), 4 * m, &mut flag);
 
     if flag == 0 { Ok(()) } else { Err(flag) }
 }
