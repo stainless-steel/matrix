bb04a491b476202752e13383fa1787a8c51d76c9
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-05-24 09:49:23 -0400

    Move unit tests into src

--- src/algebra.rs
+++ src/algebra.rs
@@ -54,3 +54,36 @@ pub fn multiply_add(A: &[f64], B: &[f64], C: &[f64], D: &mut [f64], m: usize, p:
         dgemm(Trans::N, Trans::N, m, n, p, 1.0, A, m, B, p, 1.0, D, m);
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use assert;
+
+    #[test]
+    fn multiply() {
+        let (m, p, n) = (2, 4, 1);
+
+        let A = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
+        let B = vec![1.0, 2.0, 3.0, 4.0];
+        let mut C = vec![0.0, 0.0];
+
+        super::multiply(&A, &B, &mut C, m, p, n);
+
+        assert::equal(&C, &vec![50.0, 60.0]);
+    }
+
+    #[test]
+    fn multiply_add() {
+        let (m, p, n) = (2, 3, 4);
+
+        let A = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0];
+        let B = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0, 11.0, 12.0];
+        let C = vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0];
+        let mut D = vec![0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0];
+
+        super::multiply_add(&A, &B, &C, &mut D, m, p, n);
+
+        assert::equal(&C, &vec![1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0]);
+        assert::equal(&D, &vec![23.0, 30.0, 52.0, 68.0, 81.0, 106.0, 110.0, 144.0]);
+    }
+}
--- tests/lib.rs
+++
@@ -1,7 +0,0 @@
-#![allow(non_snake_case)]
-
-extern crate assert;
-extern crate matrix;
-
-mod algebra;
-mod decomposition;
