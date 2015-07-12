4dcd85e07230c5ff8493070d115aa4ad6733846c
Ivan Ukhov <ivan.ukhov@gmail.com>
2015-07-12 10:51:15 -0400

    Implement Sparse for Band

--- src/band.rs
+++ src/band.rs
@@ -1,4 +1,11 @@
-use {Dense, Element};
+use {Dense, Element, Sparse};
+
+macro_rules! min(
+    ($left:expr, $right:expr) => ({
+        let (left, right) = ($left, $right);
+        if left < right { left } else { right }
+    });
+);
 
 /// A band matrix.
 ///
@@ -25,15 +32,22 @@ pub struct Band<T: Element> {
 
 matrix!(Band);
 
+impl<T: Element> Sparse for Band<T> {
+    fn nonzeros(&self) -> usize {
+        let &Band { rows, columns, superdiagonals, subdiagonals, .. } = self;
+        let mut count = 0;
+        for k in 0..(superdiagonals + 1) {
+            count += min!(columns - k, rows);
+        }
+        for k in 1..(subdiagonals + 1) {
+            count += min!(columns, rows - k);
+        }
+        count
+    }
+}
+
 impl<T: Element> From<Band<T>> for Dense<T> {
     fn from(band: Band<T>) -> Dense<T> {
-        macro_rules! min(
-            ($left:expr, $right:expr) => ({
-                let (left, right) = ($left, $right);
-                if left < right { left } else { right }
-            });
-        );
-
         let Band { rows, columns, superdiagonals, subdiagonals, ref data } = band;
 
         let diagonals = superdiagonals + 1 + subdiagonals;
@@ -64,22 +78,32 @@ impl<T: Element> From<Band<T>> for Dense<T> {
 
 #[cfg(test)]
 mod tests {
-    use {Band, Dense};
+    use {Band, Dense, Sparse};
+
+    macro_rules! new(
+        ($rows:expr, $columns:expr, $superdiagonals:expr, $subdiagonals:expr, $data:expr) => (
+            Band {
+                rows: $rows,
+                columns: $columns,
+                superdiagonals: $superdiagonals,
+                subdiagonals: $subdiagonals,
+                data: $data,
+            }
+        );
+        ($rows:expr, $columns:expr, $superdiagonals:expr, $subdiagonals:expr) => (
+            new!($rows, $columns, $superdiagonals, $subdiagonals,
+                 vec![0.0; ($superdiagonals + 1 + $subdiagonals) * $columns])
+        );
+    );
 
     #[test]
     fn into_tall_dense() {
-        let band = Band {
-            rows: 7,
-            columns: 4,
-            superdiagonals: 2,
-            subdiagonals: 2,
-            data: vec![
-                0.0,  0.0,  1.0,  4.0,  8.0,
-                0.0,  2.0,  5.0,  9.0, 12.0,
-                3.0,  6.0, 10.0, 13.0, 15.0,
-                7.0, 11.0, 14.0, 16.0, 17.0,
-            ],
-        };
+        let band = new!(7, 4, 2, 2, vec![
+            0.0,  0.0,  1.0,  4.0,  8.0,
+            0.0,  2.0,  5.0,  9.0, 12.0,
+            3.0,  6.0, 10.0, 13.0, 15.0,
+            7.0, 11.0, 14.0, 16.0, 17.0,
+        ]);
 
         let dense: Dense<f64> = band.into();
 
@@ -93,21 +117,15 @@ mod tests {
 
     #[test]
     fn into_wide_dense() {
-        let band = Band {
-            rows: 4,
-            columns: 7,
-            superdiagonals: 2,
-            subdiagonals: 2,
-            data: vec![
-                 0.0,  0.0,  1.0,  4.0,  8.0,
-                 0.0,  2.0,  5.0,  9.0, 13.0,
-                 3.0,  6.0, 10.0, 14.0,  0.0,
-                 7.0, 11.0, 15.0,  0.0,  0.0,
-                12.0, 16.0,  0.0,  0.0,  0.0,
-                17.0,  0.0,  0.0,  0.0,  0.0,
-                 0.0,  0.0,  0.0,  0.0,  0.0,
-            ],
-        };
+        let band = new!(4, 7, 2, 2, vec![
+             0.0,  0.0,  1.0,  4.0,  8.0,
+             0.0,  2.0,  5.0,  9.0, 13.0,
+             3.0,  6.0, 10.0, 14.0,  0.0,
+             7.0, 11.0, 15.0,  0.0,  0.0,
+            12.0, 16.0,  0.0,  0.0,  0.0,
+            17.0,  0.0,  0.0,  0.0,  0.0,
+             0.0,  0.0,  0.0,  0.0,  0.0,
+        ]);
 
         let dense: Dense<f64> = band.into();
 
@@ -120,5 +138,26 @@ mod tests {
             0.0, 0.0,  0.0, 17.0,
             0.0, 0.0,  0.0,  0.0,
         ]);
+    }
+
+    #[test]
+    fn nonzeros() {
+        assert_eq!(new!(4, 4, 0, 0).nonzeros(), 4);
+        assert_eq!(new!(4, 4, 1, 0).nonzeros(), 7);
+        assert_eq!(new!(4, 4, 2, 0).nonzeros(), 9);
+        assert_eq!(new!(4, 4, 0, 1).nonzeros(), 7);
+        assert_eq!(new!(4, 4, 0, 2).nonzeros(), 9);
+
+        assert_eq!(new!(4, 5, 0, 0).nonzeros(), 4);
+        assert_eq!(new!(4, 5, 1, 0).nonzeros(), 8);
+        assert_eq!(new!(4, 5, 2, 0).nonzeros(), 11);
+        assert_eq!(new!(4, 5, 0, 1).nonzeros(), 7);
+        assert_eq!(new!(4, 5, 0, 2).nonzeros(), 9);
+
+        assert_eq!(new!(5, 4, 0, 0).nonzeros(), 4);
+        assert_eq!(new!(5, 4, 1, 0).nonzeros(), 7);
+        assert_eq!(new!(5, 4, 2, 0).nonzeros(), 9);
+        assert_eq!(new!(5, 4, 0, 1).nonzeros(), 8);
+        assert_eq!(new!(5, 4, 0, 2).nonzeros(), 11);
     }
 }
